ced71e96b189ded1451b8150d19056a58c9239c1
Nathan Rebours <nathan.p.rebours@gmail.com>
2019-09-18 18:09:05 +0200

    Implement File_path on top of Astlib

--- src/file_path.ml
+++ src/file_path.ml
@@ -1,4 +1,5 @@
 open! Import
+open Astlib.V4_07
 
 let get_default_path (loc : Location.t) =
   let fname = loc.loc_start.pos_fname in
@@ -7,12 +8,24 @@ let get_default_path (loc : Location.t) =
   | None       -> fname
 ;;
 
-let get_default_path_str : structure -> string = function
-  | [] -> ""
-  | { pstr_loc = loc; _ } :: _ -> get_default_path loc
+let get_default_path_str str =
+  match Option.value_exn (Structure.to_concrete str) with
+  | Structure { a = [] } -> ""
+  | Structure { a = stri :: _ } ->
+    match Option.value_exn (Structure_item.to_concrete stri) with
+    | Structure_item { pstr_loc = loc; _ } -> get_default_path loc
 ;;
 
-let get_default_path_sig : signature -> string = function
-  | [] -> ""
-  | { psig_loc = loc; _ } :: _ -> get_default_path loc
+let get_default_path_sig sig_ =
+  match Option.value_exn (Signature.to_concrete sig_) with
+  | Signature { a = [] } -> ""
+  | Signature { a = sigi :: _ } ->
+    match Option.value_exn (Signature_item.to_concrete sigi) with
+    | Signature_item { psig_loc = loc; _ } -> get_default_path loc
 ;;
+
+let get_default_path_str str =
+  get_default_path_str (Astlib.Conversions.structure_to_ast str)
+
+let get_default_path_sig sig_ =
+  get_default_path_sig (Astlib.Conversions.signature_to_ast sig_)
